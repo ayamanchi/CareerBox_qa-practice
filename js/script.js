2e8c88050408d395e66f63cd7f5d7f0c6ede019e
ayamanchi <ayamanchi@gmail.com>
2023-05-22 02:16:16 +0600

    redact css

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    loop:true,
-    margin: 0 ,
-    autoplay:true,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true
-     responsive: {
-            0: { 
-                items: 2
-            }
-            340: {
-                items: 2
-            },
-            1000: {
-                items: 5
-            }
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        340: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        }
 
-        }
+    }
 });
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
 })
-$('.stop').on('click',function(){
+$('.stop').on('click', function () {
     owl.trigger('stop.owl.autoplay')
 })
